4dfa2d2d4438643bfef7c6f8fcd320745cd3e765
HvNelric <37754127+HvNelric@users.noreply.github.com>
2022-05-04 22:46:48 +0200

    2e

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Welcome from './components/welcome/Welcome';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
+    return (
+    <Welcome />
   );
 }
 
